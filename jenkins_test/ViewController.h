85b8f81de5f7de5b4c3b7fe14b6dabc315dcd807
尚晓楠 <shangxiaonan@126.com>
2016-12-01 23:53:08 +0800

    create

---
+++ jenkins_test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  jenkins_test
+//
+//  Created by Xiaonan Shang on 16/12/1.
+//  Copyright © 2016年 xinhuaruide. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ jenkins_test/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  jenkins_test
+//
+//  Created by Xiaonan Shang on 16/12/1.
+//  Copyright © 2016年 xinhuaruide. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
